a64941a001f167a49950ec3aeb79442a81b7e229
BreakinBenny <fox@blaj.net>
2026-05-03 17:03:54 +0200

    Shutdown2 updated with spawnpoints based on your class
    Much like Schtop!
    
    Offensive classes are closer to the entry, Defense ones are closer to the flag
    
    Co-Authored-By: hi there <109190436+hi07there@users.noreply.github.com>

--- maps/ff_shutdown2.lua
+++ maps/ff_shutdown2.lua
@@ -37,3 +37,18 @@ end
 -----------------------------------------------------------------------------
 blue_slayer = not_red_trigger:new()
 red_slayer = not_blue_trigger:new()
+
+-----------------------------------------------------------------------------
+-- OFFENSE AND DEFENSE SPAWNS
+-----------------------------------------------------------------------------
+red_o_only = function(self,player) return ((player:GetTeamId() == Team.kRed) and ((player:GetClass() == Player.kScout) or (player:GetClass() == Player.kSoldier) or (player:GetClass() == Player.kMedic) or (player:GetClass() == Player.kPyro) or (player:GetClass() == Player.kSpy))) end
+red_d_only = function(self,player) return ((player:GetTeamId() == Team.kRed) and ((player:GetClass() == Player.kSniper) or (player:GetClass() == Player.kSoldier) or (player:GetClass() == Player.kDemoman) or (player:GetClass() == Player.kHwguy) or (player:GetClass() == Player.kPyro) or (player:GetClass() == Player.kEngineer))) end
+
+redspawn_offense = { validspawn = red_o_only }
+redspawn_defense = { validspawn = red_d_only }
+
+blue_o_only = function(self,player) return ((player:GetTeamId() == Team.kBlue) and ((player:GetClass() == Player.kScout) or (player:GetClass() == Player.kSoldier) or (player:GetClass() == Player.kMedic) or (player:GetClass() == Player.kPyro) or (player:GetClass() == Player.kSpy))) end
+blue_d_only = function(self,player) return ((player:GetTeamId() == Team.kBlue) and ((player:GetClass() == Player.kSniper) or (player:GetClass() == Player.kSoldier) or (player:GetClass() == Player.kDemoman) or (player:GetClass() == Player.kHwguy) or (player:GetClass() == Player.kPyro) or (player:GetClass() == Player.kEngineer))) end
+
+bluespawn_offense = { validspawn = blue_o_only }
+bluespawn_defense = { validspawn = blue_d_only }
